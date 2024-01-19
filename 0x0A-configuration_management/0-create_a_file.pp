63a3b030ef08d01376c76a655a443918094f4aba
mohanned-sameh <mohanned2678@gmail.com>
2024-01-19 18:27:34 +0200

    Update file creation in Puppet manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# creates a file with the content "I Love Puppet" in /tmp/school
+# creates a file with the content "I Love Puppet"
+# in /tmp/school
 
 file { '/tmp/school':
-  ensure  => file,
   content => 'I Love Puppet',
   mode    => '0744',
   owner   => 'www-data',
