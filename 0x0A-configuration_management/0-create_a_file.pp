a45866ce2fb1950a3096a303e00fa9ac95c65a64
mohanned-sameh <mohanned2678@gmail.com>
2024-01-19 18:28:03 +0200

    Update content of file in /tmp/school

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# creates a file with the content "I Love Puppet"
-# in /tmp/school
+# creates a file in /tmp
 
 file { '/tmp/school':
-  content => 'I Love Puppet',
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
