92a1ae8ee212b08826182c3291289c0cefe4ef56
mohanned-sameh <mohanned2678@gmail.com>
2024-01-19 18:34:51 +0200

    Update package installation method for flask to use specific version (2.1.0) with pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask 2.1.0 using pip3
-
-exec { 'install_flask':
-	command => '/usr/local/bin/pip3 install flask==2.1.0',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
