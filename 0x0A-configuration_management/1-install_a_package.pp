a7660c90021c11372b024cfa6c17d40c9b8ec91f
mohanned-sameh <mohanned2678@gmail.com>
2024-01-19 18:45:42 +0200

    Update flask version to 2.1.0 in puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,8 @@
 # Install an especific version of flask (2.1.0) in puppet
 
 exec { 'install_flask':
-    command => 'pip install flask==2.1.0',
-    path    => '/usr/local/bin',
-    creates => '/usr/local/bin/flask',
-    require => Package['python3-pip'],
+  command => 'pip install flask==2.1.0',
+  path    => '/usr/local/bin',
+  creates => '/usr/local/bin/flask',
+  require => Package['python3-pip'],
 }
