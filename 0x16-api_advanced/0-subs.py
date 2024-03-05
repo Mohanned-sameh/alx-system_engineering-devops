d2e98b22a4e80bc02c963cca210e77d0a86080f0
mohanned-sameh <mohanned2678@gmail.com>
2024-03-06 00:18:20 +0200

    Refactor number_of_subscribers function to use a single request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Function to return the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
+    sub_info = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(),
+        headers={"User-Agent": "Mozilla/5.0"},
+        allow_redirects=False,
+    )
+    if sub_info.status_code != 200:
         return 0
-    return response.json().get("data").get("subscribers")
+    return sub_info.json().get("data").get("subscribers")
