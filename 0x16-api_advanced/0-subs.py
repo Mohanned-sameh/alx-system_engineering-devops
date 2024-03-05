ac56f84d808c70ce5d645ae75816ee6c089b4075
mohanned-sameh <mohanned2678@gmail.com>
2024-03-06 00:19:30 +0200

    Refactor number_of_subscribers function to request data from an API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and returns the number of subscribers"""
+"""Module to request data from an API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function to return the number of subscribers"""
-    sub_info = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(),
-        headers={"User-Agent": "Mozilla/5.0"},
-        allow_redirects=False,
-    )
-    if sub_info.status_code != 200:
+    """Function to return the number of subscribers of a subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
-    return sub_info.json().get("data").get("subscribers")
+    return response.json().get("data").get("subscribers")
