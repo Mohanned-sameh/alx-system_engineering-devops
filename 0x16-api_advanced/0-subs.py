31b728a42c9bdd5bdf51c6ccec1f86b06d13f0fa
mohanned-sameh <mohanned2678@gmail.com>
2024-03-06 00:28:43 +0200

    Fix error handling in number_of_subscribers function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,6 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "u/Responsible_Peace_80"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code >= 300:
         return 0
     return response.json().get("data").get("subscribers")
