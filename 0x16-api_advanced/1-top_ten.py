7e27b350ef012c36ff565fd406a05e2872c002de
mohanned-sameh <mohanned2678@gmail.com>
2024-03-06 00:25:55 +0200

    Refactor code to improve performance and readability

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""use reddit api to print the top ten hot posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """print the top ten hot posts"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "u/Responsible_Peace_80"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json().get("data").get("children")
+    for i in range(10):
+        print(data[i].get("data").get("title"))
