4a9e7b9e4153683dd7e8e2e309c9a0528de6d691
mohanned-sameh <mohanned2678@gmail.com>
2024-03-06 00:16:02 +0200

    Refactor number_of_subscribers function to improve readability and update User-Agent header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-def number_of_subscribers(subreddit):
-    """queries the Reddit API and returns the number of subscribers"""
-    import requests
+"""Module to query the Reddit API and returns the number of subscribers"""
+import requests
 
+
+def number_of_subscribers(subreddit):
+    """Function to return the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My User Agent 1.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
