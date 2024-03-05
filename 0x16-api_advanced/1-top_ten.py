e10329760e587be994f5bef818e41c0d2bd8ecc3
mohanned-sameh <mohanned2678@gmail.com>
2024-03-06 00:28:54 +0200

    Fix response status code check in top_ten function

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "u/Responsible_Peace_80"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code >= 200:
         print("None")
         return
     data = response.json().get("data").get("children")
